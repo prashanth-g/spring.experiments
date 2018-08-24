2c8d5c60ba9f312251c95b1a76e2817bf8e8be34
prashanth-g <prashanthcsgce@gmail.com>
2018-08-24 23:31:22 +0530

    Added service functions

--- src/main/java/com/prashanth/spring/transaction/service/MovieService.java
+++ src/main/java/com/prashanth/spring/transaction/service/MovieService.java
@@ -1,16 +1,31 @@
 package com.prashanth.spring.transaction.service;
 
+import com.prashanth.spring.transaction.model.Movie;
+import com.prashanth.spring.transaction.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class MovieService {
 
     @Autowired
+    MovieRepository movieRepository;
+
+    @Autowired
     CastService castService;
 
+    @Transactional
     public void createMovie() {
+        Movie movie = new Movie();
+        movie.setMovieName("VTV");
+        movie.setYear(2010);
+        try {
+            // TODO: Save CAST
+        } catch (Exception ex) {
 
+        }
     }
 
 }
